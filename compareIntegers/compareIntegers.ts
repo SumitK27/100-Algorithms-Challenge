e83e2a005a8877d59568e5eea97989f38d7a12e5
SumitK27 <sumitkajbaje@gmail.com>
2021-11-08 23:52:15 +0530

    Compare Integers

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,22 @@
 export function compareIntegers(a: string, b: string): string {
+    // Both a and b's length should be greater than or equal to 1 and less than or equal to 10
+    if (a.length >= 1 && a.length <= 10 && b.length >= 1 && b.length <= 10) {
+        // Convert string inputs to integers
+        let intA: number = parseInt(a);
+        let intB: number = parseInt(b);
 
+        if (intA < intB) {
+            return "lesser";
+        } else if (intA > intB) {
+            return "greater";
+        } else {
+            return "equal";
+        }
+    }
+
+    return "Invalid";
 }
 
-// console.log(compareIntegers('12', '13'));
-// console.log(compareIntegers('875', '799'));
-// console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers("12", "13"));
+console.log(compareIntegers("875", "799"));
+console.log(compareIntegers("1000", "1000"));
