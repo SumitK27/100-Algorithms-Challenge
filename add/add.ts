b4ccb33fe6cc885e9cb7d7927530b15f1bd2ab42
SumitK27 <sumitkajbaje@gmail.com>
2021-11-08 20:04:27 +0530

    Solution for Add

--- add/add.ts
+++ add/add.ts
@@ -1,8 +1,19 @@
-export function add(param1: number, param2: number): number {}
+export function add(param1: number, param2: number): number {
+    return param1 + param2;
+}
 
-export function add2(...param1: number[]): number {}
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+export function add2(...param1: number[]): number {
+    let sum: number = 0;
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+    for (let i: number = 0; i < param1.length; i++) {
+        sum += param1[i];
+    }
+
+    return sum;
+}
+
+console.log(add(1, 2));
+console.log(add(3, 2));
+
+console.log(add2(1, 2, 3, 4, 5));
+console.log(add2(2, 3));
