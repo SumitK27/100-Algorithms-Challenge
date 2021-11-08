c23b294923e37fadfb595ac07227c5fbbb53dd6d
SumitK27 <sumitkajbaje@gmail.com>
2021-11-08 23:15:13 +0530

    Check Palindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,21 @@
 export function checkPalindrome(inputString: string): boolean {
-
+    let totalLength: number = inputString.length;
+    if (inputString.length % 2 === 0) {
+        for (let i: number = 0; i < totalLength / 2; i++) {
+            if (inputString[i] !== inputString[totalLength - 1 - i]) {
+                return false;
+            }
+        }
+    } else {
+        for (let i: number = 0; i < totalLength - 1 / 2; i++) {
+            if (inputString[i] !== inputString[totalLength - 1 - i]) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
-// console.log(checkPalindrome('aabaa'));
-// console.log(checkPalindrome('abac'));
-// console.log(checkPalindrome('a'));
+console.log(checkPalindrome("aabaa"));
+console.log(checkPalindrome("abac"));
+console.log(checkPalindrome("a"));
