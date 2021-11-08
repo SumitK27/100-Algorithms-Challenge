7f996e1567523ab00e90dd4db18448dd5a232c51
SumitK27 <sumitkajbaje@gmail.com>
2021-11-08 20:12:50 +0530

    Solved AddTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,3 +1,26 @@
-export function addTwoDigits(n: any): number {}
+export function addTwoDigits(n: any): number {
+    // Convert input to an integer
+    let num: number = parseInt(n);
 
-// console.log(addTwoDigits(29));
+    let sum: number = 0;
+
+    // Continue only if its a two digit number
+    if (typeof num === "number" && num >= 10 && num <= 99) {
+        // Loop until num isn't zero
+        while (num !== 0) {
+            // Add last digit to the sum
+            sum += num % 10;
+
+            // Remove last digit from the number
+            num = Math.floor(num / 10);
+        }
+
+        return sum;
+    }
+
+    return -1;
+}
+
+console.log(addTwoDigits(29));
+console.log(addTwoDigits("29"));
+console.log(addTwoDigits(1539));
