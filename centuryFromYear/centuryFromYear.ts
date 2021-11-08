248ce5bf3a4dc12f6f1489829ed1de7702e0e265
SumitK27 <sumitkajbaje@gmail.com>
2021-11-08 23:00:08 +0530

    Century from Year

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,18 @@
 export function centuryFromYear(year: number): number {
-
+    // Constraint: 1 <= Year <= 2005
+    if (year >= 1 && year <= 2005) {
+        // eg. 2003 =>
+        // => floor((2003 - 1) / 100) + 1
+        // => floor(2002 / 100) + 1
+        // => floor(20.02) + 1
+        // => 20 + 1
+        // => 21
+        return Math.floor((year - 1) / 100) + 1;
+    }
+    return -1;
 }
 
-// console.log(centuryFromYear(1905));
-// console.log(centuryFromYear(1700));
+console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1700));
+console.log(centuryFromYear(2001));
+console.log(centuryFromYear(2021));
