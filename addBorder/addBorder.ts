cc698e5b7f3b2a446da61b4a10aa902c10465a0d
SumitK27 <sumitkajbaje@gmail.com>
2021-12-24 14:02:40 +0530

    Solution for Add Borders

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,18 @@
 export function addBorder(picture: string[]): string[] {
+    // Create a border of the length of the array string + 2 (for both sides)
+    const wall: string = "*".repeat(picture[0].length + 2);
 
+    // Add the borders on the top and bottom of the array
+    picture.unshift(wall);
+    picture.push(wall);
+
+    // Create a border on both the sides of the strings
+    for (let i: number = 1; i < picture.length - 1; i++) {
+        picture[i] = "*".concat(picture[i], "*");
+    }
+
+    // Return array with border
+    return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
