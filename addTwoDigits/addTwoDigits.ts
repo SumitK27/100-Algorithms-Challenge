312604cda7bfaf5fdb8163189f56f3af3efb9aef
SumitK27 <sumitkajbaje@gmail.com>
2021-12-24 14:11:09 +0530

    Solutions for Add Two Digits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -24,3 +24,28 @@ export function addTwoDigits(n: any): number {
 console.log(addTwoDigits(29));
 console.log(addTwoDigits("29"));
 console.log(addTwoDigits(1539));
+
+// Solution Two
+
+function addTwoDigits2(n: any): number {
+    const numbers = n.toString().split("");
+
+    return numbers.reduce((a: string, b: string) => {
+        return parseInt(a) + parseInt(b);
+    });
+}
+
+console.log(addTwoDigits2(29));
+console.log(addTwoDigits2("29"));
+console.log(addTwoDigits2(1539));
+
+// Solution Three
+
+function addTwoDigits3(n: any): number {
+    const numbers = n.toString().split("");
+
+    return parseInt(numbers[0]) + parseInt(numbers[1]);
+}
+
+console.log(addTwoDigits3(29));
+console.log(addTwoDigits3("29"));
