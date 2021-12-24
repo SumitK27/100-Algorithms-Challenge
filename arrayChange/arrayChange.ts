93be5b3c62ba01120bd92e744692b13e43a3f27c
SumitK27 <sumitkajbaje@gmail.com>
2021-12-24 16:30:26 +0530

    Added Comments

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,25 @@
 export function arrayChange(inputArray: number[]): number {
+    let count = 0;
 
+    for (let i = 0; i < inputArray.length; i++) {
+        // if current value is greater than next value
+        if (inputArray[i] >= inputArray[i + 1]) {
+            // difference between what next number should be and what next number actually is
+            const difference = inputArray[i] + 1 - inputArray[i + 1];
+
+            // Assign the next number one greater than current number
+            inputArray[i + 1] = inputArray[i] + 1;
+
+            // Increment the count needed to make the next number greater by one increment per move. ie. difference is the moves needed
+            count += difference;
+        }
+    }
+
+    console.log(inputArray);
+
+    return count;
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([5, 3, 1]));
+console.log(arrayChange([1, 5, 1]));
