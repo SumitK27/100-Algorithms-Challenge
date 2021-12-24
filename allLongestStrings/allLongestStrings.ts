7dbf0c04ad73852e67df0893766efe70717d3560
SumitK27 <sumitkajbaje@gmail.com>
2021-12-24 14:28:27 +0530

    Solution for All Longest Strings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,19 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+    let longestLength = inputArray[0].length;
+    let longestWords = [];
 
+    // Get the longest
+    inputArray.forEach((element) => {
+        longestLength < element.length ? element.length : longestLength;
+    });
+
+    inputArray.forEach((element) => {
+        if (element.length === longestLength) {
+            longestWords.push(element);
+        }
+    });
+
+    return longestWords;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
