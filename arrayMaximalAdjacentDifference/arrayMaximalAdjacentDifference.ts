cfa7012c2722680e454e0b07ce29c15fd17b1247
SumitK27 <sumitkajbaje@gmail.com>
2021-12-25 15:53:18 +0530

    Solution for maximum array difference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,16 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+    let maxDiff = Math.abs(inputArray[1] - inputArray[0]);
 
+    for (let i = 2; i < inputArray.length; i++) {
+        let currentDiff = Math.abs(inputArray[i] - inputArray[i - 1]);
+
+        if (currentDiff > maxDiff) {
+            maxDiff = currentDiff;
+        }
+    }
+
+    return maxDiff;
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 9, 1, 0]));
