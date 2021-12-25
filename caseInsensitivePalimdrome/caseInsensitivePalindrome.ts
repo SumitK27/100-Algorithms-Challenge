fa0ae295efea09161a4d242f4e907a7c3d89639d
SumitK27 <sumitkajbaje@gmail.com>
2021-12-25 17:34:22 +0530

    Solutions for case insensitive palindrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,24 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
+    let original = inputString.toLowerCase().split("");
+    let reversed = [...original].reverse();
 
+    if (reversed.join("") === original.join("")) {
+        return true;
+    }
+
+    return false;
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome("AaBaa"));
+console.log(isCaseInsensitivePalindrome("abac"));
+
+// Solution Two
+export function isCaseInsensitivePalindrome2(inputString: string): boolean {
+    const originalLowerCase = inputString.toLowerCase();
+    const reversedWord = originalLowerCase.split("").reverse().join("");
+
+    return originalLowerCase === reversedWord;
+}
+
+console.log(isCaseInsensitivePalindrome2("AaBaa"));
+console.log(isCaseInsensitivePalindrome2("abac"));
