31497835745595f0b537856a76ece62bbc01119f
SumitK27 <sumitkajbaje@gmail.com>
2021-12-25 16:14:16 +0530

    Solutions for array replace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,36 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+export function arrayReplace(
+    inputArray: number[],
+    elemToReplace: number,
+    substitutionElem: number
+): number[] {
+    let outputArray: number[] = [];
 
+    inputArray.forEach((element) => {
+        if (element === elemToReplace) {
+            outputArray.push(substitutionElem);
+        } else {
+            outputArray.push(element);
+        }
+    });
+
+    return outputArray;
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
+
+// Solution Two
+export function arrayReplace2(
+    inputArray: number[],
+    elemToReplace: number,
+    substitutionElem: number
+): number[] {
+    inputArray.forEach((element, index) => {
+        if (element === elemToReplace) {
+            inputArray[index] = substitutionElem;
+        }
+    });
+
+    return inputArray;
+}
+
+console.log(arrayReplace2([1, 2, 1], 1, 3));
