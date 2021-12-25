4ef834bb85aa7750385d493868bc8c254e89d74e
SumitK27 <sumitkajbaje@gmail.com>
2021-12-25 17:38:13 +0530

    Solution 2 for century from year

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -16,3 +16,18 @@ console.log(centuryFromYear(1905));
 console.log(centuryFromYear(1700));
 console.log(centuryFromYear(2001));
 console.log(centuryFromYear(2021));
+
+export function centuryFromYear2(year: number): number {
+    const century = year / 100;
+
+    if (year % 100 === 0) {
+        return century;
+    }
+
+    return Math.floor(century + 1);
+}
+
+console.log(centuryFromYear2(1905));
+console.log(centuryFromYear2(1700));
+console.log(centuryFromYear2(2001));
+console.log(centuryFromYear2(2021));
