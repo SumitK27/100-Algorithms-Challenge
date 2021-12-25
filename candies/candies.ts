b046605d3e7b1b7658cfa9d6804889cc93791f7c
SumitK27 <sumitkajbaje@gmail.com>
2021-12-25 17:15:31 +0530

    Solution for candies

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,8 @@
 export function candies(n: number, m: number): number {
+    let candy = Math.floor(m / n) * n;
 
+    return candy;
 }
 
-// console.log(candies(3, 10));
+console.log(candies(3, 10));
+console.log(candies(4, 15));
